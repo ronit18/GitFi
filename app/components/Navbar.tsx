bda0a31b308eb50044df9d03b42833926a35177c
RonitGandhi <ronitgandhi96@gmail.com>
2023-06-20 16:11:55 +0530

    Created Navbar layout and ui and Added Logo of github.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,25 @@
-import { Box, Flex } from '@/app/chakra/chakra';
+import { Box, Button, Flex } from '@/app/chakra/chakra';
+import { Image } from '@/app/chakra/chakra-next';
 import React from 'react';
 
 const Navbar = () => {
 	return (
 		<Flex justifyContent={'space-between'} py={6} alignItems={'center'}>
-			<Box>Home</Box>
+			<Box position={'relative'} aspectRatio={5 / 3} minHeight={20}>
+				<Image
+					src={'/logo.png'}
+					alt="github logo"
+					fill
+					sx={{
+						filter: 'invert(1)',
+					}}
+				/>
+			</Box>
+			<Box>
+				<Button size="md" colorScheme="whatsapp">
+					Search History
+				</Button>
+			</Box>
 		</Flex>
 	);
 };
