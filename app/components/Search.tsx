6947af1672d44779a57cf8546aeed3f04cc7efcc
RonitGandhi <ronitgandhi96@gmail.com>
2023-06-21 10:56:18 +0530

    Creating UserProfile Component and passed the data from fetch using useState.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,15 +1,22 @@
 'use client';
 import React, { useState } from 'react';
 import { Button, Input, useToast } from '@/app/chakra/chakra';
+import UserProfile from './UserProfile';
 
 const Search = () => {
 	const [query, setQuery] = useState('');
+
+	const [userData, setUserData] = useState(null);
+
+	const [loading, setLoading] = useState(false);
 
 	const toast = useToast();
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
 		if (!query) return;
+		setLoading(true);
+		setUserData(null);
 		try {
 			const res = await fetch(`https://api.github.com/users/${query}`);
 			const data = await res.json();
@@ -25,6 +32,7 @@ const Search = () => {
 					isClosable: true,
 				});
 			}
+			setUserData(data);
 		} catch (err: any) {
 			console.log('Error in components/Search.tsx: ', err);
 			return toast({
@@ -34,28 +42,33 @@ const Search = () => {
 				duration: 3000,
 				isClosable: true,
 			});
+		} finally {
+			setLoading(false);
 		}
 	};
 
 	return (
-		<form action="submit" onSubmit={handleSubmit}>
-			<Input
-				onChange={(e) => setQuery(e.target.value)}
-				variant={'outline'}
-				placeholder={'Type a username (i.e. @ronit18)'}
-				focusBorderColor="green.500"
-			/>
-			<Button
-				disabled={!query}
-				opacity={!query ? 0.5 : 1}
-				size="md"
-				type="submit"
-				colorScheme="whatsapp"
-				mt={4}
-			>
-				Search
-			</Button>
-		</form>
+		<>
+			<form action="submit" onSubmit={handleSubmit}>
+				<Input
+					onChange={(e) => setQuery(e.target.value)}
+					variant={'outline'}
+					placeholder={'Type a username (i.e. @ronit18)'}
+					focusBorderColor="green.500"
+				/>
+				<Button
+					disabled={!query}
+					opacity={!query ? 0.5 : 1}
+					size="md"
+					type="submit"
+					colorScheme="whatsapp"
+					mt={4}
+				>
+					Search
+				</Button>
+			</form>
+			{userData && <UserProfile userData={userData} loading={loading} />}
+		</>
 	);
 };
 
